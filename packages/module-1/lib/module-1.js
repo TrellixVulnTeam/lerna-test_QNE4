698a985697f77d17af3cd91986c799c8009cbbf7
Levine Hua <951995489@qq.com>
2021-11-26 11:20:48 +0800

    test

---
+++ packages/module-1/lib/module-1.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: 华松林
+ * @Date: 2021-11-26 10:30:47
+ * @LastEditors: 华松林
+ * @LastEditTime: 2021-11-26 11:12:31
+ * @FilePath: /lerna-test/packages/module-1/lib/module-1.js
+ */
+'use strict';
+
+module.exports = module1;
+
+function module1() {
+    // TODO
+    console.log('我是一个测试 lerna');
+}
